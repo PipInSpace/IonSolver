4617cd3c9e8d90ac65c28294e11465fb6b7bf39b
Julius <jupster18@gmx.de>
2023-02-24 17:11:38 +0100

    implement diffuse and add_source

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,38 @@
 use micro_ndarray::Array;
 
-
 type Vec2 = (f64, f64);
 
 struct Engine {
-    particles: Array<f64, 2>,
-    velocity: Array<Vec2, 2>,
-    gravity: f64,
+    u: Array<f64, 2>,
+    velo: Array<f64, 2>,
+    u_prev: Array<f64, 2>,
+    velo_prev: Array<f64, 2>,
+    dens: Array<f64, 2>,
+    dens_prev: Array<f64, 2>,
 }
 
 impl Engine {
-    pub fn step(&mut self) {
-        
+    pub fn add_source(&mut self, s: Array<f64, 2>, dt: f64) {
+        for ([x, y], item) in self.dens.iter_mut() {
+            *item += dt * s[[x, y]];
+        }
     }
+
+    pub fn diffuse(&mut self, diff: f64, dt: f64) {
+        let a = dt * diff * (self.dens.size().iter().map(|x| *x as f64).product::<f64>());
+        let d = self.dens.clone();
+        for k in 0..20 {
+            for (([x, y], item), (_, item_old)) in self.dens.iter_mut().zip(self.dens_prev.iter()) {
+                *item = *item_old
+                    + a * (d[[x - 1, y]] + d[[x + 1, y]] + d[[x, y - 1]] + d[[x, y + 1]])
+                        / (1.0 + 4.0 * a);
+            }
+        }
+    }
+
+    pub fn step_density(&mut self) {}
+
+    pub fn step(&mut self) {}
 }
 
-fn main() {
-    Engine {
-        particles: Array::new([200, 200]),
-        velocity: Array::new([200, 200]),
-        gravity: 1.0,
-    }.step();
-}
+fn main() {}
