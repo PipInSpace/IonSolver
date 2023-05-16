e68a3f1f159cb60ef2de9c3ac1d00df3664615bb
Julius <jupster18@gmx.de>
2023-05-16 21:42:30 +0200

    further improvements

--- src/solver.rs
+++ src/solver.rs
@@ -22,6 +22,29 @@ pub fn add_source(x: &mut Array<f64, 2>, s: &mut Array<f64, 2>, dt: f64) {
     }
 }
 
+/// Adds sources, used in the density and velocity steps. aka does things. Vec2 variant.
+pub fn add_source_vec2(x: &mut Array<Vec2, 2>, s: &mut Array<Vec2, 2>, dt: f64) {
+    // Adds sources, used in the density and velocity steps
+    // Can also be called independently of those
+    for (c, item) in x.iter_mut() {
+        x[c].x += dt * s[c].x;
+        x[c].y += dt * s[c].y;
+    }
+}
+
+/// Primes the lin_solve() function for diffusing grid cells
+pub fn diffuse(
+    s: &SimSize,
+    b: i32,
+    x: &mut Array<f64, 2>,
+    x0: &mut Array<f64, 2>,
+    diff: f64,
+    dt: f64,
+) {
+    let a = dt * diff * s.x as f64 * s.y as f64;
+    lin_solve(&s, b, x, x0, a, 1.0 + 4.0 * a)
+}
+
 /// Primes the lin_solve() function for diffusing grid cells
 pub fn diffuse(
     s: &SimSize,
@@ -54,9 +77,10 @@ pub fn project(
             force_prev[[i, j]].x = 0.0;
         }
     }
-    // Combine
-    set_bnd(&s, 0, force_y_prev);
-    set_bnd(&s, 0, force_x_prev);
+    // Combined
+    //set_bnd(&s, 0, force_y_prev);
+    //set_bnd(&s, 0, force_x_prev);
+    set_bnd_vec2(s, 0, 0, force);
 
     lin_solve(&s, 0, force_x_prev, force_y_prev, 1.0, 4.0);
 
@@ -69,9 +93,10 @@ pub fn project(
         }
     }
 
-    // Combine
-    set_bnd(&s, 1, force_x);
-    set_bnd(&s, 2, force_y);
+    // Combinen
+    //set_bnd(&s, 1, force_x);
+    //set_bnd(&s, 2, force_y);
+    set_bnd_vec2(s, 1, 2, force);
 }
 
 /// Uses Gauss-Seidel relaxation to solve a system of linear equations.
@@ -134,6 +159,7 @@ pub fn advect(
 }
 
 pub fn set_bnd(s: &SimSize, b: i32, x: &mut Array<f64, 2>) {
+    // b: 1 means x is being processed, 2 means y
     for i in 1..=s.x {
         x[[i, 0]] = if b == 2 { -x[[i, 1]] } else { x[[i, 1]] };
         x[[i, s.y + 1]] = if b == 2 { -x[[i, s.y]] } else { x[[i, s.y]] };
@@ -150,8 +176,29 @@ pub fn set_bnd(s: &SimSize, b: i32, x: &mut Array<f64, 2>) {
 }
 
 pub fn set_bnd_vec2(s: &SimSize, mode_x: i32, mode_y: i32, x: &mut Array<Vec2, 2>){
-    
-    set_bnd(s, mode_x, x);
+    // mode is unneeded, x is always 1, y is always 2
+
+    for i in 1..=s.x {
+        x[[i, 0]].x = if mode_x == 2 { -x[[i, 1]].x } else { x[[i, 1]].x };
+        x[[i, 0]].y = if mode_y == 2 { -x[[i, 1]].y } else { x[[i, 1]].y };
+        x[[i, s.y + 1]].x = if mode_x == 2 { -x[[i, s.y]].x } else { x[[i, s.y]].x };
+        x[[i, s.y + 1]].y = if mode_y == 2 { -x[[i, s.y]].y } else { x[[i, s.y]].y };
+    }
+    for i in 1..=s.y {
+        x[[0, i]].x = if mode_x == 1 { -x[[1, i]].x } else { x[[1, i]].x };
+        x[[0, i]].y = if mode_y == 1 { -x[[1, i]].y } else { x[[1, i]].y };
+        x[[s.x + 1, i]].x = if mode_x == 1 { -x[[s.x, i]].x } else { x[[s.x, i]].x };
+        x[[s.x + 1, i]].y = if mode_y == 1 { -x[[s.x, i]].y } else { x[[s.x, i]].y };
+    }
+
+    x[[0, 0]].x = 0.5 * (x[[1, 0]].x + x[[0, 1]].x);
+    x[[0, 0]].y = 0.5 * (x[[1, 0]].y + x[[0, 1]].y);
+    x[[0, s.y + 1]].x = 0.5 * (x[[1, s.y + 1]].x + x[[0, s.y]].x);
+    x[[0, s.y + 1]].y = 0.5 * (x[[1, s.y + 1]].y + x[[0, s.y]].y);
+    x[[s.x + 1, 0]].x = 0.5 * (x[[s.x, 0]].x + x[[s.x + 1, 1]].x);
+    x[[s.x + 1, 0]].y = 0.5 * (x[[s.x, 0]].y + x[[s.x + 1, 1]].y);
+    x[[s.x + 1, s.y + 1]].x = 0.5 * (x[[s.x, s.y + 1]].x + x[[s.x + 1, s.y]].x);
+    x[[s.x + 1, s.y + 1]].y = 0.5 * (x[[s.x, s.y + 1]].y + x[[s.x + 1, s.y]].y);
 }
 
 pub fn dens_step(
@@ -177,9 +224,10 @@ pub fn vel_step(
     visc: f64,
     dt: f64,
 ) {
-    // combine
-    add_source(force_x, force_x_prev, dt);
-    add_source(force_y, force_y_prev, dt);
+    // Combined
+    //add_source(force_x, force_x_prev, dt);
+    //add_source(force_y, force_y_prev, dt);
+    add_source_vec2(force, force_prev, dt);
 
     // Combined
     swap(force_prev, force);
