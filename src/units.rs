017debdfef2b7e4e257402cc3a59399df387adb7
Finn <finn294@gmx.de>
2023-10-31 17:25:32 +0100

    rename force unit

--- src/units.rs
+++ src/units.rs
@@ -9,7 +9,7 @@ pub struct Units {
     /// coulomb
     pub c : f32,
     /// Newton
-    pub f : f32,
+    pub N : f32,
     /// coulomb's constant
     pub ke : f32,
 }
@@ -21,7 +21,7 @@ impl Units {
             kg : 1.0,
             s : 1.0,
             c : 1.0,
-            f : 1.0,
+            N : 1.0,
             ke : 1.0,
         }
     }
@@ -32,8 +32,8 @@ impl Units {
         self.s = u / si_u * self.m;
         self.c = si_c / c;
 
-        self.f = self.kg * self.m / (self.s * self.s);
-        self.ke = self.f * self.m * self.m / (self.c * self.c);
+        self.N = self.kg * self.m / (self.s * self.s);
+        self.ke = self.N * self.m * self.m / (self.c * self.c);
     }
 }
 
