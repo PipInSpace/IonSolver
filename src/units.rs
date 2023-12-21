ade8b0d9b054091835d049bbfec386c1246c6d46
PipInSpace <jupster18@gmx.de>
2023-12-21 12:38:30 +0100

    more unit conversion

--- src/units.rs
+++ src/units.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 
-/// SI Base units (Lattice unit * unit = si unit eg. lengthLU * m = length in meters)
+/// Unit conversion factors (Lattice unit * factor = si unit eg. lengthLU * m = length in meters)
 #[derive(Clone, Copy, Default)]
 #[allow(non_snake_case)]
 pub struct Units {
@@ -26,22 +26,22 @@ impl Units {
 
     pub fn set(
         &mut self,
-        x: f32,
-        u: f32,
-        rho: f32,
+        lbm_length: f32,
+        lbm_velocity: f32,
+        lbm_rho: f32,
         c: f32,
-        si_x: f32,
-        si_u: f32,
+        si_lenght: f32,
+        si_velocity: f32,
         si_rho: f32,
         si_c: f32,
     ) {
-        self.m = si_x / x;
-        self.kg = si_rho / rho * self.m * self.m * self.m;
-        self.s = u / si_u * self.m;
+        self.m = si_lenght / lbm_length;
+        self.kg = si_rho / lbm_rho * cb(self.m);
+        self.s = lbm_velocity / si_velocity * self.m;
         self.c = si_c / c;
     }
 
-    // to si from LU
+    // to si units from lattice units (need to be called after .set();)
     pub fn len_to_si(&self, l: f32) -> f32 {
         l * self.m
     }
@@ -70,8 +70,7 @@ impl Units {
         f * (self.kg * self.m / (self.s * self.s))
     }
 
-    // to LU from si
-
+    // to lattice units from si units (need to be called after .set();)
     pub fn si_to_len(&self, l: f32) -> f32 {
         l / self.m
     }
@@ -100,7 +99,14 @@ impl Units {
         f / (self.kg * self.m / (self.s * self.s))
     }
 
+    pub fn si_to_nu(&self, nu: f32) -> f32 {
+        nu * self.s / (self.m * self.m)
+    }
+
     pub fn si_to_ke(&self) -> f32 {
+        // 1 / (4 * pi * epsilon_0) = k_e
+        // epsilon_0 has the unit Farad/meter and needs to be converted to lattice units
+        // 1 / (4 * 3.14159 * (8.8541878128 * 10^-12)) = 8.987552E9 (k_e)
         8.987552E9 / (self.kg * cb(self.m) / (self.c * self.c * self.s * self.s))
     }
 }
